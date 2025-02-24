0d49c39fe209e40b209ee92d4f117f3a85786190
Roman96932423 <ddent358@gmail.com>
2025-02-24 20:29:26 +0100

    Create models for cars app and migration in DB

--- backend/car_rental/cars/admin.py
+++ backend/car_rental/cars/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from cars.models import Brand, Car, Rental
+
+
 # Register your models here.
+admin.site.register(Brand)
+admin.site.register(Car)
+admin.site.register(Rental)
+
--- backend/car_rental/cars/models.py
+++ backend/car_rental/cars/models.py
@@ -1,3 +1,29 @@
-from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import Model, CharField, DateField, ForeignKey, IntegerField, CASCADE
 
-# Create your models here.
+
+class Brand(Model):
+    name = CharField(max_length=15)
+    date_of_founding = DateField()
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Car(Model):
+    brand = ForeignKey(Brand, on_delete=CASCADE)
+    model = CharField(max_length=15)
+    year_build = DateField()
+    VIN = CharField(max_length=100)
+    mileage = IntegerField()
+
+    def __str__(self):
+        return f'{self.brand} | {self.model} | {self.year_build}'
+
+
+class Rental(Model):
+    car = ForeignKey(Car, on_delete=CASCADE)
+    # user = ForeignKey(User, on_delete=CASCADE)
+    date_rented = DateField()
+    date_returned = DateField()
+    price_per_day = IntegerField()
